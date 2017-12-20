9bc331a67cf07187bfa2074da2435503bbd1a261
Karol <kajezierski@wp.pl>
2017-12-20 19:24:33 +0000

    Add profiles controller, new view, and link on homepage

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,18 @@
+class ProfilesController < ApplicationController
+  before_action :only_curent_user, only: :new
+  # GET to /users/:user_id/profile/new
+  def new
+    # Render blank profile details form
+    ""
+  end
+  
+  private
+  
+  def only_curent_user
+    unless (user_signed_in? && params[:user_id].to_i == current_user.id)
+      #flash[:notice] = "#{current_user.id} and evalute to: #{params[:user_id].to_i == current_user.id.to_i}"
+      flash[:notice] = "You are trying to enter restricted area for you"
+      redirect_to root_url
+    end
+  end
+end
