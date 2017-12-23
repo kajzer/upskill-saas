3a2eed38d6d0cdcfc6804c70a07d23d83511de62
Karol <kajezierski@wp.pl>
2017-12-23 10:29:59 +0000

    Add users controller and show view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  
+  # GET to /users/:id
+  def show
+    @user = User.find( params[:id] )
+    @profile = Profile.find_by user_id: @user.id
+  end
+  
+end
